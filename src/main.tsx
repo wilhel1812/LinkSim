053a74857204915fbe387014cb7007939e4d0c67
wilhel1812 <wilhel1812@gmail.com>
2026-03-11 23:15:48 +0100

    Redirect 127.0.0.1 to localhost to unify browser storage

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,14 @@ import "maplibre-gl/dist/maplibre-gl.css";
 import "./index.css";
 import App from "./App";
 
+if (window.location.hostname === "127.0.0.1") {
+  const redirectUrl =
+    `${window.location.protocol}//localhost` +
+    `${window.location.port ? `:${window.location.port}` : ""}` +
+    `${window.location.pathname}${window.location.search}${window.location.hash}`;
+  window.location.replace(redirectUrl);
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
