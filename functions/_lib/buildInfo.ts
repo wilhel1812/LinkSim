d9506ab8d6af423e62762864660943cde8a0b53e
wilhel1812 <wilhel1812@gmail.com>
2026-03-13 10:15:44 +0100

    Update build metadata and staging wrangler config

--- functions/_lib/buildInfo.ts
+++ functions/_lib/buildInfo.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = "0.0.0";
-export const APP_COMMIT = "01acaed9";
+export const APP_VERSION = "0.8.0";
+export const APP_COMMIT = "62606807";
 export const APP_BUILD_LABEL = `v${APP_VERSION}+${APP_COMMIT}`;
