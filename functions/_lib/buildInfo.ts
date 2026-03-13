3fc22c61ea69c90bdd359a6a8a8da6806ec0423b
wilhel1812 <wilhel1812@gmail.com>
2026-03-13 11:43:49 +0100

    chore(release): bump app version to 0.8.1

--- functions/_lib/buildInfo.ts
+++ functions/_lib/buildInfo.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = "0.8.0";
-export const APP_COMMIT = "25da99bd";
+export const APP_VERSION = "0.8.1";
+export const APP_COMMIT = "97d58771";
 export const APP_BUILD_LABEL = `v${APP_VERSION}+${APP_COMMIT}`;
