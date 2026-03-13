59601ea343bd081f5e286c2c130234e7ba422dfb
wilhel1812 <wilhel1812@gmail.com>
2026-03-13 21:19:58 +0100

    release: bump version to 0.9.5

--- src/lib/buildInfo.ts
+++ src/lib/buildInfo.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = "0.9.4";
-export const APP_COMMIT = "a280d28f";
+export const APP_VERSION = "0.9.5";
+export const APP_COMMIT = "ae2393b3";
 export const APP_BUILD_LABEL = `v${APP_VERSION}+${APP_COMMIT}`;
