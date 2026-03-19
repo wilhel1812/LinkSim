10f9b00e32565c4881e6a1b05e214dbdeca6434b
wilhel1812 <wilhel1812@gmail.com>
2026-03-19 22:59:49 +0100

    0.9.18

--- functions/_lib/buildInfo.ts
+++ functions/_lib/buildInfo.ts
@@ -1,5 +1,5 @@
-export const APP_VERSION = "0.9.15";
-export const APP_COMMIT = "5b981b24";
+export const APP_VERSION = "0.9.18";
+export const APP_COMMIT = "acda1882";
 export const APP_BUILD_LABEL = `v${APP_VERSION}+${APP_COMMIT}`;
 export type BuildChannel = "stable" | "beta" | "alpha";
 export const buildLabelForChannel = (channel: BuildChannel): string => {
