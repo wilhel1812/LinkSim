53ecbafe1e09be5a7c33749f979535ce51554e21
wilhel1812 <wilhel1812@gmail.com>
2026-04-08 08:27:03 +0200

    Add live in-app UI gallery route using real styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { AppShell } from "./components/AppShell";
+import { UiGalleryPage } from "./components/UiGalleryPage";
 
 function App() {
+  if (window.location.pathname === "/ui-gallery") {
+    return <UiGalleryPage />;
+  }
   return <AppShell />;
 }
 
